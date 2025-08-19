edaccccec8d92a9745add2ef5773c568f9917cce
Rmonofee1122 <nakanishi_r@monofee.co.jp>
2025-08-19 13:16:10 +0900

    next.config.jsに以下を追加 typescript: {     ignoreBuildErrors: true,   },

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },

2ab6a237fa249725087fa88806a635a50b3803b7
Rmonofee1122 <nakanishi_r@monofee.co.jp>
2025-09-08 14:41:48 +0900

    Remove next.config.js file as part of project cleanup.

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // traceファイルの出力を固定IDに変更してWindowsの権限エラーを回避
-  generateBuildId: () => {
-    return "build";
-  },
-};
-
-module.exports = nextConfig;
